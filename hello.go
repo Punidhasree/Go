13148dd1efe9a53aaa88a3b21e90c98889f171cb
Punidhasree <punidhasreesekar@gmail.com>
2025-11-24 11:23:17 +0530

    greetings for multiple people

--- hello.go
+++ hello.go
@@ -3,9 +3,25 @@ package main
 import (
     "fmt"
     "example.com/greetings"
+    "log"
 )
 
 func main() {
-    message := greetings.Hello("Punidha")
-    fmt.Println(message)
+    // Set properties of the predefined Logger, including
+    // the log entry prefix and a flag to disable printing
+    // the time, source file, and line number.
+    log.SetPrefix("greetings: ")
+    log.SetFlags(0)
+
+    names := [] string{ "Punidha", "pavan", "Vanidha"}
+    messages, err := greetings.Hellos(names)
+    // If an error was returned, print it to the console and
+    // exit the program.
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    // If no error was returned, print the returned message
+    // to the console.
+    fmt.Println(messages)
 }
